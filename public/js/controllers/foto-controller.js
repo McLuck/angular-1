af4e36d9626a5ca638521a7c05bb1b772470cd49
Lucas Israel <Lucas.Silva@compsisnet.com.br>
2017-01-22 16:57:50 -0200

    Aula 2 iniciada

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,36 +1,11 @@
-angular.module('projeto')
-    .controller('FotoController', function($scope, $routeParams, $location, FotoService) {
+angular.module('alurapic').controller('FotosController', function($scope){
+	$scope.foto = {
+		url : 'http://www.fundosanimais.com/Minis/leoes.jpg',
+		title : 'Leão'
+	};
 
-        $scope.foto = {};
-        $scope.mensagem = '';
-    
-        if($routeParams.fotoId) {
-            FotoService.get({fotoId: $routeParams.fotoId}, function(retorno) {
-                 $scope.foto = retorno;
-            });
-        }
-
-        $scope.submeter = function() {
-           if($scope.formulario.$valid) {
-                if(!$routeParams.fotoId) {
-                    
-                    FotoService.save($scope.foto, function(_id) {
-                        $scope.foto = {};
-                        $scope.mensagem = 'Salvo com sucesso';
-                    }, function(status) {
-                        console.log(status);
-                        $scope.mensagem = 'Não foi possível salvar';
-                    });
-
-                } else {
-                    FotoService.update({fotoId : $scope.foto._id}, $scope.foto, function(_id) {
-                        $scope.foto = {};
-                        $scope.mensagem = 'Alterado com sucesso';                        
-                    }, function(status) {
-                        console.log(status);
-                        $scope.mensagem = 'Não foi possível alterar';
-                    });                    
-                }
-           } 
-        };
-    });
+	$scope.foto2 = {
+		url : 'http://www.fundosanimais.com/Minis/leoes.jpg',
+		title : 'Leão 2'
+	};
+});
